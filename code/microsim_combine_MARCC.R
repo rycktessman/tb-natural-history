aa0620b01d0cc5a1e1ad42ddc07fc16e2f2ce380
rycktessman <34560948+rycktessman@users.noreply.github.com>
2022-08-31 10:47:38 -0400

    rrfree output, updates to run microsom_combine for all start_pops at once

--- code/microsim_combine_MARCC.R
+++ code/microsim_combine_MARCC.R
@@ -7,10 +7,8 @@ library(data.table)
 
 country <- as.character(Sys.getenv('country'))
 analysis <- as.character(Sys.getenv('analysis'))
-start_pop <- as.numeric(Sys.getenv('start_pop')) #1=smear-/symptom-, 2=smear+/symptom-, 3=smear-/symptom+, 4=smear+/symptom+
 print(country)
 print(analysis)
-print(start_pop)
 
 #update path
 path_out <- paste0("output/", tolower(country), "_", analysis, "/")
@@ -26,26 +24,37 @@ stats <- c("mean", "med", "q25", "q75", "lb", "ub")
 #find, load, combine files
 setwd(path_out)
 
-#1. time_all
-completed <- list.files(pattern=paste0("times_all_", start_pop, "_"))
-print(completed)
-times_all_all <- Map(rda2list, completed)
-times_all_comb <- list()
-for(i in stats) {
-  print(i)
-  times_all_comb[[i]] <- bind_rows(lapply(1:length(completed), function(x) times_all_all[[x]][[i]]))
+start_pop <- 1:4
+for(s in start_pop) {
+  print(s)
+  #1. time_all
+  completed <- list.files(pattern=paste0("times_all_", s, "_"))
+  print(completed)
+  times_all_all <- Map(rda2list, completed)
+  times_all_comb <- list()
+  for(i in stats) {
+    print(i)
+    times_all_comb[[i]] <- bind_rows(lapply(1:length(completed), function(x) times_all_all[[x]][[i]]))
+  }
+  save(times_all_comb, file=paste0("times_all_comb", s, ".Rda"))
+  
+  #2. proportions
+  completed <- list.files(pattern=paste0("props_", s, "_"))
+  print(completed)
+  props_all <- Map(rda2list, completed)
+  props_comb <- bind_rows(props_all)
+  write.csv(props_comb, file=paste0("props_comb", s, ".csv"), row.names=F)
+  
+  #3. state distribution for figures
+  completed <- list.files(pattern=paste0("sim_ind_all_", s, "_"))
+  print(completed)
+  sim_ind_all_comb <- lapply(completed, fread)
+  save(sim_ind_all_comb, file=paste0("sim_ind_all_comb", s, ".Rda"))
+  
+  rm(sim_ind_all_comb)
+  rm(props_all)
+  rm(props_comb)
+  rm(times_all_comb)
+  rm(times_all_all)
+  gc()
 }
-save(times_all_comb, file=paste0("times_all_comb", start_pop, ".Rda"))
-
-#2. proportions
-completed <- list.files(pattern=paste0("props_", start_pop, "_"))
-print(completed)
-props_all <- Map(rda2list, completed)
-props_comb <- bind_rows(props_all)
-write.csv(props_comb, file=paste0("props_comb", start_pop, ".csv"), row.names=F)
-
-#3. state distribution for figures
-completed <- list.files(pattern=paste0("sim_ind_all_", start_pop, "_"))
-print(completed)
-sim_ind_all_comb <- lapply(completed, fread)
-save(sim_ind_all_comb, file=paste0("sim_ind_all_comb", start_pop, ".Rda"))
